abf59a086401ed4e53a10cf81e27ec44e4587804
abi <massive2bdamnedfan@gmail.com>
2026-06-24 17:00:20 +0100

    Update model.lua

--- render/model.lua
+++ render/model.lua
@@ -6,8 +6,10 @@ local is3D = {is3D}
 
 game:GetObjects("http://{domain}/asset/?id={requestId}&time="..tostring(math.random()))[1].Parent = workspace
 
+local renderType = "PNG"
+
 if is3D then
-	return (game:GetService("ThumbnailGenerator"):Click("OBJ", 420, 420, true, true))
-else
-	return (game:GetService("ThumbnailGenerator"):Click("PNG", 420, 420, true, true))
+	renderType = "OBJ"
 end
+
+return (game:GetService("ThumbnailGenerator"):Click(renderType, 420, 420, true, true))
